33079f05d2b4c9545b58f7b10b5cf5772d46ab92
Oleksandr Yarovyi <b00168592@mytudublin.ie>
2026-02-18 22:10:05 +0000

    added limit to 7 pages in scrapper

--- scrape.py
+++ scrape.py
@@ -37,48 +37,48 @@ def searchIE(searchrequest):
 
     #get products
     while allProducts != []:
-        pagenum += 1
-        url = f'https://www.amazon.ie/s?k={searchrequest}&page={pagenum}'
-        result = session.get(url)
-        page = BeautifulSoup(result.content, "lxml")
-        allProducts = page.find_all("div", role="listitem")
-        for product in allProducts:
-            try:
-                innerText = product.find("h2", class_="a-size-base-plus a-spacing-none a-color-base a-text-normal")
-                title = innerText.find("span").text
+        #print(pagenum)
+        if allProducts != [] and pagenum <= 7:
+            pagenum += 1
+            url = f'https://www.amazon.ie/s?k={searchrequest}&page={pagenum}'
+            result = session.get(url)
+            page = BeautifulSoup(result.content, "lxml")
+            allProducts = page.find_all("div", role="listitem")
+            for product in allProducts:
+                try:
+                    innerText = product.find("h2", class_="a-size-base-plus a-spacing-none a-color-base a-text-normal")
+                    title = innerText.find("span").text
 
-            except:
-                title = "None"
+                except:
+                    title = "None"
 
 
-            try:
-                price = product.find("span", class_="a-price-whole").text + product.find("span",class_="a-price-fraction").text
-            except:
-                price = "None"
+                try:
+                    price = product.find("span", class_="a-price-whole").text + product.find("span",class_="a-price-fraction").text
+                except:
+                    price = "None"
 
-            try:
-                image = product.find("img", class_="s-image").attrs["src"]
-            except:
-                image = "None"
-            link = product.find("a")["href"]
-            match = re.search(r'/dp/([A-Z0-9]{10})', link)
+                try:
+                    image = product.find("img", class_="s-image").attrs["src"]
+                except:
+                    image = "None"
+                link = product.find("a")["href"]
+                match = re.search(r'/dp/([A-Z0-9]{10})', link)
 
-            newProductObject = productObject.copy()
+                newProductObject = productObject.copy()
 
-            newProductObject["title"] = title
-            newProductObject["price"] = price
-            newProductObject["image"] = image
+                newProductObject["title"] = title
+                newProductObject["price"] = price
+                newProductObject["image"] = image
 
-            if match:
-                asin = match.group(1)
-                newProductObject["link"] = "https://www.amazon.ie/dp/" + asin
-                newProductObject["asin"] = asin
+                if match:
+                    asin = match.group(1)
+                    newProductObject["link"] = "https://www.amazon.ie/dp/" + asin
+                    newProductObject["asin"] = asin
 
-            resultsList.append(newProductObject)
+                resultsList.append(newProductObject)
 
         time.sleep(sleepTime)
 
     resultJSON = json.dumps(resultsList, indent=1)
     return resultJSON
-
-print(searchIE("RTX 5070"))
